5f5a8185d5b4b53eef4f7da4462d088620c57008
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-05 17:16:22 +0530

    new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+import {FaBars} from "react-icons/fa"
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <Link to={"/"}>
+            <h1>Bhaskar.</h1>
+        </Link>
+        <ul className='nav-menu'>
+            <li>
+                <Link to="/">Home</Link>
+                
+            </li>
+            <li>
+            <Link to="/projects">Projects</Link>
+                
+            </li>
+
+            <li>
+            <Link to="/about">About</Link>
+            </li>
+
+            <li>
+               <Link to="/contact">Contact</Link>
+
+            </li>
+        </ul>
+        <div className="hamburger">
+            <FaBars size={20} style={{color: "#fff" }}/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
