fd7f05ac9ea0ff3ae37abde3d3777140ea18d9b0
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-05 15:35:59 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./routes/About"
+import Contact from "./routes/Contact"
+import Projects from "./routes/Projects"
+import Home from "./routes/Home"
+
+function App() {
+  return (
+    <>
+    <Routes>
+      
+        <Route path="/" element={<Home/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      
+    </Routes>
+    </>
+  );
+}
+
+export default App;
