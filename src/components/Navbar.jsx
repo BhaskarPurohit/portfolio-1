dd4b0cb288cce5ee484c5eb93993a800afcf6443
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-05 17:22:55 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./Navbar.css"
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {FaBars} from "react-icons/fa"
 
