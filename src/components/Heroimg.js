2592f4f9ad67566606075e6fcbe1d6f287c587f7
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-06 23:35:57 +0530

    new

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./heroimg.css";
+import bgImg from "../assets/pngwing.com.png"
+import { Link } from 'react-router-dom';
 
 const Heroimg = () => {
   return (
-    <div>Heroimg</div>
+    <div className="hero">
+        <div className="mask">
+            <img className="into-img" src={bgImg} alt='IntroImg'/>
+        </div>
+        <div className="content">
+            <p>Hi, I'm a Web Developer</p>
+            <h1>MERN Stack Developer.</h1>
+            <div>
+                <Link to="/projects" className="btn">Projects</Link>
+                <Link to="/contact" className="btn-light">Contact</Link>
+                </div>
+        </div>
+
+    </div>
   )
 }
 
